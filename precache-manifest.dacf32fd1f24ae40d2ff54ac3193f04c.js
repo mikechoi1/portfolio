576729bbb60aabbaf6eed93a36eac5c1644bb5f3
mikechoi1 <befuddledmike@gmail.com>
2019-04-22 13:56:16 -0700

    Updates

---
+++ precache-manifest.dacf32fd1f24ae40d2ff54ac3193f04c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7b5c95d89f168b82a86",
+    "url": "/static/css/main.43a501df.chunk.css"
+  },
+  {
+    "revision": "a7b5c95d89f168b82a86",
+    "url": "/static/js/main.8d1ff4f1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8fbdedab7b890781cd0a",
+    "url": "/static/js/2.96dd47ad.chunk.js"
+  },
+  {
+    "revision": "e6190f851aae8e42d208ad48fa45b083",
+    "url": "/static/media/nailpalace.e6190f85.png"
+  },
+  {
+    "revision": "de24c19300df1de5b48fc096844e9bd9",
+    "url": "/static/media/flower.de24c193.jpeg"
+  },
+  {
+    "revision": "a6ffab9c16863d9cc12be751e34bdebf",
+    "url": "/static/media/cat.a6ffab9c.png"
+  },
+  {
+    "revision": "eb5947c61bbe25b5989e24d675468046",
+    "url": "/static/media/project-background.eb5947c6.png"
+  },
+  {
+    "revision": "c1f579a86c98bd96a0304a0bd98ae704",
+    "url": "/index.html"
+  }
+];
