0f0e2bb98efbbb2ecb6a8d3db70745b2df582326
mikechoi1 <befuddledmike@gmail.com>
2019-08-21 14:14:16 -0700

    Updates

---
+++ precache-manifest.a9aba9fc7d269397cebf739b3b118575.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8edb39c4dce79d763688de222930b000",
+    "url": "/static/media/sliding-puzzle-ai.8edb39c4.png"
+  },
+  {
+    "revision": "eb5947c61bbe25b5989e24d675468046",
+    "url": "/static/media/project-background.eb5947c6.png"
+  },
+  {
+    "revision": "e6190f851aae8e42d208ad48fa45b083",
+    "url": "/static/media/nailpalace.e6190f85.png"
+  },
+  {
+    "revision": "2d215e2190889bd3810c39df1312e871",
+    "url": "/static/media/n-queen-problem-ai.2d215e21.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bc1f3b03aa69e01ffb38",
+    "url": "/static/js/main.777e879f.chunk.js"
+  },
+  {
+    "revision": "8fbdedab7b890781cd0a",
+    "url": "/static/js/2.96dd47ad.chunk.js"
+  },
+  {
+    "revision": "bc1f3b03aa69e01ffb38",
+    "url": "/static/css/main.6e90dd85.chunk.css"
+  },
+  {
+    "revision": "d455f6eaf7d9b605b7421f9243fc2803",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6131cba5b7eaba722d102dfc916ce910.js"
+  "/precache-manifest.a9aba9fc7d269397cebf739b3b118575.js"
 );
 
 workbox.clientsClaim();
